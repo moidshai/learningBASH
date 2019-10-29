3bb10fd23ed89c5c44c476a6e087dd9d859cf706
mosh <moidshaikh125@gmail.com>
2019-10-30 02:02:30 +0530

    first commit

---
+++ firstbash.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# A simple bash script to print helloworld
+
+# # # # # # # # # # # # # # # 
+# Special variables in bash #
+# # # # # # # # # # # # # # # 
+
+# $0 Name of the bash script
+
+echo $0
+
+# $1-9 First 9 arguments to the script
+
+echo $0
+
+# $0 Name of the bash script
+
+echo $0
+
+# $0 Name of the bash script
+
+echo $0
+
+# $0 Name of the bash script
+
+echo $0
+
+# $0 Name of the bash script
+
+echo $0
+
+# $0 Name of the bash script
+
+echo $0
+
